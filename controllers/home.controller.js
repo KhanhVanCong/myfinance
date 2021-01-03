00a440cf98f88c25f361b6904c4a5736b696ece9
khanhvancong <khanhvc2310@gmail.com>
2021-01-03 15:25:37 +0700

    Fix bug sort Updated At in table financial and Invest

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,51 +1,41 @@
 const Finance = require('../models/financial.model');
 const Invest = require('../models/invest.model');
+const { Op } = require("sequelize");
+const sequelize = require('sequelize');
 
 exports.getDashboard = async (req, res, next) => {
   const userId = req.user.id;
-  const totalIncome = await Finance.sum('money', {
-    where: {
-      userId,
-      categoryFinancialId: 1
-    }
-  });
-  const totalIncomeByCash = await Finance.sum('money', {
-    where: {
-      userId,
-      categoryFinancialId: 1,
-      paymentMethodId: 1
-    }
-  });
-  const totalExpense = await Finance.sum('money', {
-    where: {
-      userId,
-      categoryFinancialId: 2
-    }
-  });
-  const totalExpenseByCash = await Finance.sum('money', {
-    where: {
-      userId,
-      categoryFinancialId: 2,
-      paymentMethodId: 1
-    }
-  });
-  const totalDept = await Finance.sum('money', {
-    where: {
-      userId,
-      categoryFinancialId: 3
-    }
-  });
-  const totalInvest = await Invest.sum('money', { where: { userId } });
-  const totalInvestByCash = await Invest.sum('money', {
-    where:
-      {
-        userId,
-        paymentMethodId: 1
-      }
-  });
+  const [ totalIncome,
+          totalIncomeByCash,
+          totalExpense,
+          totalExpenseByCash,
+          totalDept,
+          totalInvest,
+          totalInvestByCash ] = await Promise.all([
+    getTotalIncome(userId),
+    getTotalIncomeByCash(userId),
+    getTotalExpense(userId),
+    getTotalExpenseByCash(userId),
+    getTotalDept(userId),
+    getTotalInvest(userId),
+    getTotalInvestByCash(userId)
+  ]);
+
   const totalMoneyAvailable = totalIncome - totalExpense - totalInvest;
   const totalMoneyAvailableByCash = totalIncomeByCash - totalExpenseByCash - totalInvestByCash;
   const totalMoneyAvailableByBank = totalMoneyAvailable - totalMoneyAvailableByCash;
+
+  // Data chart for Income
+  // const incomesInYear = await Finance.findAll({
+  //   attributes: [
+  //     [sequelize.fn('sum', sequelize.col('money')), 'totalMoney'],
+  //   ],
+  //   where: {
+  //     categoryFinancialId: 1
+  //   },
+  //   group: [sequelize.fn('date_trunc', 'year', sequelize.col('date'))]
+  // });
+
   res.render('home/dashboard', {
     path: '/dashboard',
     pageTitle: "Dashboard",
@@ -58,3 +48,72 @@ exports.getDashboard = async (req, res, next) => {
     totalMoneyAvailableByBank
   });
 };
+
+function getTotalIncome(userId) {
+  return Finance.sum('money', {
+    where: {
+      userId,
+      categoryFinancialId: 1
+    }
+  })
+}
+
+function getTotalIncomeByCash(userId) {
+  return Finance.sum('money', {
+    where: {
+      userId,
+      categoryFinancialId: 1,
+      paymentMethodId: 1
+    }
+  })
+}
+
+function getTotalExpense(userId) {
+  return Finance.sum('money', {
+    where: {
+      userId,
+      categoryFinancialId: 2
+    }
+  })
+}
+
+function getTotalExpenseByCash(userId) {
+  return Finance.sum('money', {
+    where: {
+      userId,
+      categoryFinancialId: 2,
+      paymentMethodId: 1
+    }
+  })
+}
+
+function getTotalDept(userId) {
+  return Finance.sum('money', {
+    where: {
+      userId,
+      categoryFinancialId: 3
+    }
+  })
+}
+
+function getTotalInvest(userId) {
+  return Invest.sum('money', { where: { userId } })
+}
+
+function getTotalInvestByCash(userId) {
+  return Invest.sum('money', {
+    where:
+      {
+        userId,
+        paymentMethodId: 1
+      }
+  })
+}
+
+function getIncomesInYear(userId) {
+  return req.user.getFinancials({
+    where: {
+      categoryFinancialId: 1
+    }
+  })
+}
--- public/js/pages/chart-apex.js
+++ public/js/pages/chart-apex.js
@@ -1,53 +1,125 @@
 'use strict';
 
-$(document)
-  .ready(function () {
+$(document).ready(function () {
     setTimeout(() => {
-      $(function () {
+      $(function() {
         var options = {
           chart: {
             height: 350,
-            type: 'line',
+            type: 'bar',
           },
-          series: [ {
-            name: 'Website Blog',
-            type: 'column',
-            data: [ 440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160 ]
-          }, {
-            name: 'Social Media',
-            type: 'line',
-            data: [ 23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16 ]
-          } ],
-          stroke: {
-            width: [ 0, 4 ]
-          },
-          colors: [ "#4680ff", "#ff5252" ],
-          title: {
-            text: 'Traffic Sources'
-          },
-          labels: [ '01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001' ],
-          xaxis: {
-            type: 'datetime'
-          },
-          yaxis: [ {
-            title: {
-              text: 'Website Blog',
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              columnWidth: '55%',
+              endingShape: 'rounded'
             },
-
+          },
+          dataLabels: {
+            enabled: false
+          },
+          colors: ["#0e9e4a", "#4680ff", "#ff5252"],
+          stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent']
+          },
+          series: [{
+            name: 'Net Profit',
+            data: [44, 55, 57, 56, 61, 58, 63]
           }, {
-            opposite: true,
+            name: 'Revenue',
+            data: [76, 85, 101, 98, 87, 105, 91]
+          }, {
+            name: 'Free Cash Flow',
+            data: [35, 41, 36, 26, 45, 48, 52]
+          }],
+          xaxis: {
+            categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
+          },
+          yaxis: {
             title: {
-              text: 'Social Media'
+              text: '$ (thousands)'
             }
-          } ]
+          },
+          fill: {
+            opacity: 1
 
+          },
+          tooltip: {
+            y: {
+              formatter: function(val) {
+                return "$ " + val + " thousands"
+              }
+            }
+          }
         }
         var chart = new ApexCharts(
-          document.querySelector("#mixed-chart-1"),
+          document.querySelector("#bar-chart-1"),
           options
         );
         chart.render();
       });
+
+      $(function() {
+        var options = {
+          chart: {
+            height: 350,
+            type: 'bar',
+          },
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              columnWidth: '55%',
+              endingShape: 'rounded'
+            },
+          },
+          dataLabels: {
+            enabled: false
+          },
+          colors: ["#0e9e4a", "#4680ff", "#ff5252"],
+          stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent']
+          },
+          series: [{
+            name: 'Net Profit',
+            data: [44, 55, 57, 56, 61, 58, 63]
+          }, {
+            name: 'Revenue',
+            data: [76, 85, 101, 98, 87, 105, 91]
+          }, {
+            name: 'Free Cash Flow',
+            data: [35, 41, 36, 26, 45, 48, 52]
+          }],
+          xaxis: {
+            categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
+          },
+          yaxis: {
+            title: {
+              text: '$ (thousands)'
+            }
+          },
+          fill: {
+            opacity: 1
+
+          },
+          tooltip: {
+            y: {
+              formatter: function(val) {
+                return "$ " + val + " thousands"
+              }
+            }
+          }
+        }
+        var chart = new ApexCharts(
+          document.querySelector("#bar-chart-2"),
+          options
+        );
+        chart.render();
+      });
+
       $(function () {
         var options = {
           chart: {
@@ -82,6 +154,7 @@ $(document)
         );
         chart.render();
       });
+
       $(function () {
         var options = {
           chart: {
@@ -131,4 +204,67 @@ $(document)
         chart.render();
       });
     }, 700);
+
+  function renderChart(idChart, nam, dataYearNow, dataYearBefore, unit) {
+    var options = {
+      series: [{
+        name: `${nam}`,
+        data: dataYearNow
+      }, {
+        name: `${nam - 1}`,
+        data: dataYearBefore
+      }],
+      chart: {
+        id: `${idChart}`,
+        type: 'bar',
+        height: 250
+      },
+      colors: ["#ff5252", "#4680ff"],
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '55%',
+          endingShape: 'flat'
+        },
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent']
+      },
+      xaxis: {
+        categories: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        labels: {
+          rotate: -45,
+          trim: false,
+        }
+      },
+      yaxis: {
+        title: {
+          text: `${unit}`
+        },
+        labels: {
+          formatter: function (val, index) {
+            return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+          }
+        }
+      },
+      fill: {
+        opacity: 1
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + `${unit}`
+          }
+        }
+      }
+    };
+    const chart = new ApexCharts(document.querySelector(`#${idChart}`), options);
+    chart.render();
+  };
+
   });
